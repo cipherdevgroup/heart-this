b3b49780db859dae35464298d7f5e499c038d9d1
robneu <rob@wpsitecare.com>
2016-07-22 10:38:11 -0400

    Cleaned up widget code a bit
    Related to #8

--- includes/widgets/class-popular-posts.php
+++ includes/widgets/class-popular-posts.php
@@ -13,39 +13,120 @@
  */
 class Heart_This_Popular_Posts extends WP_Widget {
 
-	function __construct() {
-		parent::__construct(
-			'heart_this_widget',
-			__( 'Heart This - Popular Posts', 'heart-this' ),
+	/**
+	 * Holds widget settings defaults, populated in the child class' constructor.
+	 *
+	 * @var array
+	 */
+	protected $defaults;
+
+	/**
+	 * Register widget information and default settings.
+	 *
+	 * @since  0.1.0
+	 * @access public
+	 * @return void
+	 */
+	public function __construct( $id_base = false, $name = false, $widget_options = array(), $control_options = array() ) {
+		$this->id_base = ( $id_base ) ? $id_base : 'heart-this-widget';
+		$this->name    = ( $name ) ? $name : __( 'HeartThis - Popular Posts', 'heart-this' );
+
+		$this->defaults = array(
+			'title'         => __( 'Popular Posts', 'heart-this' ),
+			'description'   => '',
+			'posts'         => 1,
+			'display_count' => 1,
+		);
+
+		$widget_options = wp_parse_args(
+			$widget_options,
 			array(
+				'classname'   => 'heart-this-widget heart-this-popular-posts',
 				'description' => __( 'Displays your most popular posts sorted by most liked', 'heart-this' ),
 			)
 		);
+
+		$control_options = wp_parse_args( $control_options, array() );
+
+		parent::__construct( $this->id_base, $this->name, $widget_options, $control_options );
 	}
 
-	function widget( $args, $instance ) {
-		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? __( 'Popular Posts', 'heart-this' ) : $instance['title'], $instance, $this->id_base );
+	/**
+	 * Display the widget content.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @param  array $args Display arguments including before_title, after_title, before_widget, and after_widget.
+	 * @param  array $instance The settings for the particular instance of the widget.
+	 * @return void
+	 */
+	public function widget( $args, $instance ) {
+		$instance = wp_parse_args( (array) $instance, $this->defaults );
+
+		$title = apply_filters( 'widget_title', $title, $instance['title'], $this->id_base );
 		$posts = empty( $instance['posts'] ) ? 1 : $instance['posts'];
 
 		require HEART_THIS_DIR . 'includes/widgets/display-popular-posts.php';
 	}
 
-	function update( $new_instance, $old_instance ) {
-		$new_instance['title']         = wp_strip_all_tags( $new_instance['title'] );
-		$new_instance['description']   = strip_tags( $new_instance['description'], '<a><b><strong><i><em><span>' );
-		$new_instance['posts']         = is_integer( absint( $new_instance['posts'] ) ) ? absint( $new_instance['posts']  ) : 1;
-		$new_instance['display_count'] = ( !empty( $new_instance['display_count'] ) ) ? 1 : 0;
+	/**
+	 * Update a particular instance.
+	 *
+	 * This function should check that $new_instance is set correctly.
+	 * The newly calculated value of $instance should be returned.
+	 * If "false" is returned, the instance won't be saved/updated.
+	 *
+	 * @since  1.0.0
+	 * @access public
+	 * @param  array $new_instance New settings for this instance as input by the user via form().
+	 * @param  array $old_instance Old settings for this instance.
+	 * @return array Settings to save or bool false to cancel saving
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$instance = $new_instance;
 
-		return $new_instance;
+		$instance['title']         = wp_strip_all_tags( $instance['title'] );
+		$instance['description']   = strip_tags( $instance['description'], '<a><b><strong><i><em><span>' );
+		$instance['posts']         = empty( $instance['posts'] ) ? 1 : absint( $instance['posts'] );
+		$instance['display_count'] = empty( $instance['display_count'] ) ? 0 : 1;
+
+		return $instance;
 	}
 
-	function form( $instance ) {
-		$instance = wp_parse_args( (array) $instance, array(
-			'title'         => __( 'Popular Posts', 'heart-this' ),
-			'description'   => '',
-			'posts'         => 1,
-			'display_count' => 1,
-		) );
+	/**
+	 * Output a widget field ID.
+	 *
+	 * @since  0.1.0
+	 * @access public
+	 * @param  string $name The field name to be echoed.
+	 * @return void
+	 */
+	public function field_id( $name ) {
+		echo $this->get_field_id( $name );
+	}
+
+	/**
+	 * Output a widget field name.
+	 *
+	 * @since  0.1.0
+	 * @access public
+	 * @param  string $name The field name to be echoed.
+	 * @return void
+	 */
+	public function field_name( $name ) {
+		echo $this->get_field_name( $name );
+	}
+
+	/**
+	 * Load a template to display the widget form.
+	 *
+	 * @since  0.1.0
+	 * @access public
+	 * @param  array $instance The current widget form data.
+	 * @return void
+	 */
+	public function form( $instance ) {
+		$instance = wp_parse_args( (array) $instance, $this->defaults );
 
 		require HEART_THIS_DIR . 'includes/widgets/form-popular-posts.php';
 	}
--- includes/widgets/form-popular-posts.php
+++ includes/widgets/form-popular-posts.php
@@ -10,18 +10,18 @@
 
 ?>
 <p>
-	<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php esc_html_e( 'Title:', 'heart-this' ); ?></label>
-	<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $instance['title']; ?>" />
+	<label for="<?php $this->field_id( 'title' ); ?>"><?php esc_html_e( 'Title:', 'heart-this' ); ?></label>
+	<input class="widefat" id="<?php $this->field_id( 'title' ); ?>" name="<?php $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $instance['title']; ?>" />
 </p>
 <p>
-	<label for="<?php echo $this->get_field_id( 'description' ); ?>"><?php esc_html_e( 'Description:', 'heart-this' ); ?></label>
-	<input class="widefat" id="<?php echo $this->get_field_id( 'description' ); ?>" name="<?php echo $this->get_field_name( 'description' ); ?>" type="text" value="<?php echo $instance['description']; ?>" />
+	<label for="<?php $this->field_id( 'description' ); ?>"><?php esc_html_e( 'Description:', 'heart-this' ); ?></label>
+	<input class="widefat" id="<?php $this->field_id( 'description' ); ?>" name="<?php $this->get_field_name( 'description' ); ?>" type="text" value="<?php echo $instance['description']; ?>" />
 </p>
 <p>
-	<label for="<?php echo $this->get_field_id( 'posts' ); ?>"><?php esc_html_e( 'Posts:', 'heart-this' ); ?></label>
-	<input id="<?php echo $this->get_field_id( 'posts' ); ?>" name="<?php echo $this->get_field_name( 'posts' ); ?>" type="number" min="1" value="<?php echo $instance['posts']; ?>" size="3" />
+	<label for="<?php $this->field_id( 'posts' ); ?>"><?php esc_html_e( 'Posts:', 'heart-this' ); ?></label>
+	<input id="<?php $this->field_id( 'posts' ); ?>" name="<?php $this->get_field_name( 'posts' ); ?>" type="number" min="1" value="<?php echo $instance['posts']; ?>" size="3" />
 </p>
 <p>
-	<input id="<?php echo $this->get_field_id( 'display_count' ); ?>" name="<?php echo $this->get_field_name( 'display_count' ); ?>" type="checkbox" value="1" <?php checked( $instance['display_count'] ); ?>>
-	<label for="<?php echo $this->get_field_id( 'display_count' ); ?>"><?php esc_html_e( 'Display like counts', 'heart-this' ); ?></label>
+	<input id="<?php $this->field_id( 'display_count' ); ?>" name="<?php $this->get_field_name( 'display_count' ); ?>" type="checkbox" value="1" <?php checked( $instance['display_count'] ); ?>>
+	<label for="<?php $this->field_id( 'display_count' ); ?>"><?php esc_html_e( 'Display like counts', 'heart-this' ); ?></label>
 </p>
